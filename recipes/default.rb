7e4f20d62b4dd205e65e26317aa808a7e8a1f196
miki <mikipn.88@gmaill.com>
2017-01-25 14:56:58 +0100

    new changes

--- recipes/default.rb
+++ recipes/default.rb
@@ -2,7 +2,8 @@ include_recipe 'flugel-grafana-prometheus::install'
 include_recipe 'flugel-grafana-prometheus::configure'
 include_recipe 'flugel-grafana-prometheus::default_dashboards'
 
-include_recipe 'prometheus::default'
+include_recipe 'prometheus::service' 
+
 include_recipe 'nginx::default'
 
 
